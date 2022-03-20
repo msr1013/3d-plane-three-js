6ac1106e4649f06e6e78756fe2f423313a5446dc
Manish Rawal <manish.rawal@convergetree.com>
2022-03-20 15:24:01 +0545

    modify plane with dat.gui

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
   // Find the latest version by visiting https://cdn.skypack.dev/three.
 
   import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+  import * as dat from 'dat.gui'
+  console.log(dat)
+
+  const gui = new dat.GUI()
+  const world = {
+    plane : {
+      width : 10,
+      height: 10,
+      widthSegments:10,
+      heightSegments:10
+    }
+  }
+  gui.add(world.plane,'width',1,20).
+  onChange(generatePlane)
+
+  gui.add(world.plane,'height',1,20).
+  onChange(generatePlane)
+
+  gui.add(world.plane,'widthSegments',1,20).
+  onChange(generatePlane)
+
+  gui.add(world.plane,'heightSegments',1,20).
+  onChange(generatePlane)
+
+
+  function generatePlane() {
+    planeMesh.geometry.dispose()
+    planeMesh.geometry = new THREE.PlaneGeometry(
+      world.plane.width,
+      world.plane.height,
+      world.plane.widthSegments,
+      world.plane.heightSegments)
+
+    const {array} = planeMesh.geometry.attributes.position
+    for (let i = 0; i <array.length; i += 3)  {
+      const x = array[i]
+      const y = array[i+1]
+      const z = array[i+2]
+
+      array[i + 2] = z + Math.random()
+
+    }
+  }
 
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75,innerWidth/innerHeight,0.1,1000)
@@ -14,7 +57,7 @@
 
   camera.position.z = 5
 
-  const planeGeometry = new THREE.PlaneGeometry(5,5,10,10)
+  const planeGeometry = new THREE.PlaneGeometry(10,10,10,10)
   const planeMaterial = new THREE.MeshPhongMaterial({
   color:0xff0000,
   side:THREE.DoubleSide,
@@ -31,7 +74,7 @@
     const y = array[i+1]
     const z = array[i+2]
 
-    //array[i + 2] = z + Math.random()
+    array[i + 2] = z + Math.random()
 
   }
 
