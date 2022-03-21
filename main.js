e13b20cab97fc2bb573520c97340bb6d5049e61f
Manish Rawal <manish.rawal@convergetree.com>
2022-03-21 12:19:18 +0545

    set plane to exact dimensions

--- main.js
+++ main.js
@@ -13,16 +13,16 @@
   const gui = new dat.GUI()
   const world = {
     plane : {
-      width : 10,
-      height: 10,
-      widthSegments:10,
-      heightSegments:10
+      width : 2,
+      height: 19,
+      widthSegments:17,
+      heightSegments:17
     }
   }
-  gui.add(world.plane,'width',1,20).
+  gui.add(world.plane,'width',1,50).
   onChange(generatePlane)
 
-  gui.add(world.plane,'height',1,20).
+  gui.add(world.plane,'height',1,50).
   onChange(generatePlane)
 
   gui.add(world.plane,'widthSegments',1,20).
@@ -46,8 +46,15 @@
       const z = array[i+2]
 
       array[i + 2] = z + Math.random()
-
     }
+
+    const colors = []
+    for (let i = 0; i < planeMesh.geometry.attributes.position.count;i ++) {
+    colors.push(0,0.19,0.4)
+    }
+
+    planeMesh.geometry.setAttribute('color',new THREE.BufferAttribute(new Float32Array(colors),3))
+
   }
 
   const raycaster = new THREE.Raycaster()
@@ -64,7 +71,11 @@
 
   camera.position.z = 5
 
-  const planeGeometry = new THREE.PlaneGeometry(10,10,10,10)
+  const planeGeometry = new THREE.PlaneGeometry(world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+    )
   const planeMaterial = new THREE.MeshPhongMaterial({
 
   side:THREE.DoubleSide,
@@ -91,11 +102,7 @@
     colors.push(0,0.19,0.4)
   }
 
-  console.log(colors)
-
   planeMesh.geometry.setAttribute('color',new THREE.BufferAttribute(new Float32Array(colors),3))
-
-  console.log(planeMesh.geometry.attributes)
 
   const light = new THREE.DirectionalLight(
     0xffffff,1)
