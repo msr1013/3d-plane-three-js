7323d747bc88c4255230894e2a0964762bc367a6
Manish Rawal <manish.rawal@convergetree.com>
2022-03-21 15:35:34 +0545

    add vertices movement

--- main.js
+++ main.js
@@ -10,22 +10,22 @@
   const gui = new dat.GUI()
   const world = {
     plane : {
-      width : 2,
-      height: 19,
-      widthSegments:17,
-      heightSegments:17
+      width : 400,
+      height: 400,
+      widthSegments:50,
+      heightSegments:50
     }
   }
-  gui.add(world.plane,'width',1,50).
+  gui.add(world.plane,'width',1,500).
   onChange(generatePlane)
 
-  gui.add(world.plane,'height',1,50).
+  gui.add(world.plane,'height',1,500).
   onChange(generatePlane)
 
-  gui.add(world.plane,'widthSegments',1,20).
+  gui.add(world.plane,'widthSegments',1,100).
   onChange(generatePlane)
 
-  gui.add(world.plane,'heightSegments',1,20).
+  gui.add(world.plane,'heightSegments',1,100).
   onChange(generatePlane)
 
   function generatePlane() {
@@ -36,14 +36,27 @@
       world.plane.widthSegments,
       world.plane.heightSegments)
 
-    const {array} = planeMesh.geometry.attributes.position
-    for (let i = 0; i <array.length; i += 3)  {
-      const x = array[i]
-      const y = array[i+1]
-      const z = array[i+2]
+      const {array} = planeMesh.geometry.attributes.position
+      const randomValues = []
+      for (let i = 0; i <array.length; i++)  {
+        if (i % 3 === 0) {
+          const x = array[i]
+          const y = array[i+1]
+          const z = array[i+2]
 
-      array[i + 2] = z + Math.random()
-    }
+          array[i] = x + (Math.random() - 0.5) * 3
+          array[i + 1] = y + (Math.random() - 0.5) * 3
+          array[i + 2] = z + (Math.random() - 0.5) * 3
+        }
+          randomValues.push(Math.random()*Math.PI*2)
+      }
+
+      planeMesh.geometry.attributes.position.
+      randomValues = randomValues
+
+      planeMesh.geometry.attributes.position.originalPosition = planeMesh.geometry.
+      attributes.position.array
+
 
     const colors = []
     for (let i = 0; i < planeMesh.geometry.attributes.position.count;i ++) {
@@ -66,7 +79,7 @@
 
   new OrbitControls(camera,renderer.domElement)
 
-  camera.position.z = 5
+  camera.position.z = 50
 
   const planeGeometry = new THREE.PlaneGeometry(world.plane.width,
     world.plane.height,
@@ -82,40 +95,11 @@
   const planeMesh = new THREE.Mesh(planeGeometry,planeMaterial)
   scene.add(planeMesh)
 
-
-
-  const {array} = planeMesh.geometry.attributes.position
-  const randomValues = []
-  for (let i = 0; i <array.length; i++)  {
-    if (i % 3 === 0) {
-      const x = array[i]
-      const y = array[i+1]
-      const z = array[i+2]
-
-      array[i] = x + (Math.random() - 0.5)
-      array[i + 1] = y + (Math.random() - 0.5)
-      array[i + 2] = z + Math.random()
-    }
-      randomValues.push(Math.random() - 0.5)
-  }
-
-  planeMesh.geometry.attributes.position.
-  randomValues = randomValues
-
-  planeMesh.geometry.attributes.position.originalPosition = planeMesh.geometry.
-  attributes.position.array
-
-
-  const colors = []
-  for (let i = 0; i < planeMesh.geometry.attributes.position.count;i ++) {
-    colors.push(0,0.19,0.4)
-  }
-
-  planeMesh.geometry.setAttribute('color',new THREE.BufferAttribute(new Float32Array(colors),3))
+  generatePlane()
 
   const light = new THREE.DirectionalLight(
     0xffffff,1)
-  light.position.set(0,0,1)
+  light.position.set(0,-1,1)
   scene.add(light)
 
   const backLight = new THREE.DirectionalLight(
@@ -137,10 +121,10 @@
 
     const {array,originalPosition,randomValues} = planeMesh.geometry.attributes.position
     for (let i = 0; i <array.length; i+=3) {
-      array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.003
+      array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01
 
       array[i + 1] = originalPosition[i + 1] +
-      Math.sin(frame + randomValues[i+1]) * 0.01
+      Math.sin(frame + randomValues[i+1]) * 0.001
     }
 
     planeMesh.geometry.attributes.position.needsUpdate = true
